c7210727cc38c3a404647741bb68218a1814580d
Mohamed M <mohamed613@gmail.com>
2020-02-02 17:14:57 -0500

    initial commit

---
+++ precache-manifest.6a314631f204d30653bd3a124d2dd010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc12e64c3830976ac222ced1737d51ad",
+    "url": "/image-uploader/index.html"
+  },
+  {
+    "revision": "e81e7d600ca33d2f5f28",
+    "url": "/image-uploader/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dcb6ebbbc09341f68e70",
+    "url": "/image-uploader/static/css/main.58d28432.chunk.css"
+  },
+  {
+    "revision": "e81e7d600ca33d2f5f28",
+    "url": "/image-uploader/static/js/2.b7896800.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/image-uploader/static/js/2.b7896800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb6ebbbc09341f68e70",
+    "url": "/image-uploader/static/js/main.354fa3ec.chunk.js"
+  },
+  {
+    "revision": "4e126ee2e1b56ae1e9c0",
+    "url": "/image-uploader/static/js/runtime-main.a0e66b5c.js"
+  }
+]);
